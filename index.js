b6ed5838a97ba5518a532da774fa1297be156206
Swapnil Medhe <53039172+swapnilsmedhe@users.noreply.github.com>
2022-06-29 10:24:29 +0530

    Renamed directory 'app' to 'handlers'

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { serveStaticFrom } = require('./src/app/staticFileHandler.js');
-const { guestBookHandler } = require('./src/app/guestBookHandler.js');
-const { notFoundHandler } = require('./src/app/notFoundHandler.js');
+const { serveStaticFrom } = require('./src/handlers/staticFileHandler.js');
+const { guestBookHandler } = require('./src/handlers/guestBookHandler.js');
+const { notFoundHandler } = require('./src/handlers/notFoundHandler.js');
 const { startServer } = require('./src/server/server.js');
 const { createRouter } = require('./src/server/router.js');
 
