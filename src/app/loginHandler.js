4229c8cb524c2ce94bc6849a6083020247a24200
Swapnil Medhe <53039172+swapnilsmedhe@users.noreply.github.com>
2022-07-07 10:29:03 +0530

    Extracted a funtion to redirect to guest book page

--- src/app/loginHandler.js
+++ src/app/loginHandler.js
@@ -1,5 +1,11 @@
 const { loginPage } = require('../view/loginPage.js');
 
+const redirectToGuestBook = (request, response) => {
+  response.statusCode = 302;
+  response.setHeader('Location', '/guest-book');
+  response.end();
+};
+
 const loginPageHandler = (request, response, next) => {
   const { pathname } = request.url;
   if (pathname !== '/login' || request.method !== 'GET') {
@@ -11,9 +17,7 @@ const loginPageHandler = (request, response, next) => {
     response.setHeader('Content-type', 'text/html');
     response.end(loginPage);
   } else {
-    response.statusCode = 302;
-    response.setHeader('Location', '/guest-book');
-    response.end();
+    redirectToGuestBook(request, response);
     return;
   }
 
@@ -38,9 +42,8 @@ const createLoginHandler = (sessions) => (request, response, next) => {
     const session = createSession(username);
     sessions[session.sessionId] = session;
     response.setHeader('Set-Cookie', `sessionId=${session.sessionId}`);
-    response.statusCode = 302;
-    response.setHeader('Location', '/guest-book');
-    response.end();
+
+    redirectToGuestBook(request, response);
     return;
   }
 
