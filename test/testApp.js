7f529b6b2ebad24e50a05ce900cca0cac9b2f681
Swapnil Medhe <53039172+swapnilsmedhe@users.noreply.github.com>
2022-07-13 12:26:40 +0530

    Added test for api

--- test/testApp.js
+++ test/testApp.js
@@ -1,13 +1,43 @@
+const fs = require('fs');
 const request = require('supertest');
 const { app } = require('../src/app.js');
 
 const serveFrom = './test/data';
 const dataFile = './test/data/guestBookData.json';
 
-describe('App Test', () => {
-  it('should give Not found response with 404 on GET /hello', (done) => {
+describe('GET static pages', () => {
+  it('should give Not found response with 404 on GET /hello.html', (done) => {
     request(app({ serveFrom, dataFile }))
       .get('/hello')
+      .expect('Not found')
       .expect(404, done);
+  });
+
+  it('should serve index.html on GET /', (done) => {
+    request(app({ serveFrom, dataFile }))
+      .get('/')
+      .expect(200)
+      .expect('content-type', 'text/html')
+      .expect('Index page', done);
+  });
+});
+
+describe('GET /api', () => {
+  const comments = [
+    { name: "james", comment: "Hello", timestamp: "13/07/2022 11:06:48" }
+  ]
+
+  beforeEach(() => {
+    fs.writeFileSync(dataFile, JSON.stringify(comments));
+  });
+
+  afterEach(() => fs.writeFileSync(dataFile, ''));
+
+  it('should give comments as json on GET /api/comments', (done) => {
+    request(app({ serveFrom, dataFile }))
+      .get('/api/comments')
+      .expect(200)
+      .expect('content-type', 'application/json')
+      .expect(comments, done);
   });
 });
