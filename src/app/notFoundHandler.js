774fc928a72bd920fef69d4fc85810c8d7e43808
Swapnil Medhe <53039172+swapnilsmedhe@users.noreply.github.com>
2022-07-14 20:45:30 +0530

    Deleted handlers file
    * notFoundHandler
    * parseBodyParmas
    * parseUrl
    * staticFileHandler
    * router
    * server

--- src/app/notFoundHandler.js
+++
@@ -1,7 +0,0 @@
-const notFoundHandler = (request, response) => {
-  response.statusCode = 404;
-  response.end('Not found');
-  return true;
-};
-
-module.exports = { notFoundHandler };
--- src/app/parseUrl.js
+++
@@ -1,8 +0,0 @@
-const { URL } = require('url');
-
-const parseUrl = (request, response, next) => {
-  request.url = new URL(`http://${request.headers.host}${request.url}`);
-  next();
-};
-
-module.exports = { parseUrl };
